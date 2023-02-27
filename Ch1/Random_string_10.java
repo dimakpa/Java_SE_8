e51bbee2ad432ed3c4f4ecb0efe4b838993c92ae
Dimaka <70847277+dimakpa@users.noreply.github.com>
2023-02-27 19:52:59 +0300

    Update Random_string_10.java

--- Ch1/Random_string_10.java
+++ Ch1/Random_string_10.java
@@ -1,16 +1,9 @@
-import java.util.Random;
-import java.util.Scanner;
-
 public class Random_string_10 {
     public static void main(String[] args) {
 
-        String str = "";
-        for(int i=0; i<10; i++) {
-            long generatedLong = new Random().nextLong();
-            int asc = (int) (generatedLong / 36);
-            str = str + Character.toString( (char) asc);
-        }
-        System.out.println(str);
-
+        long random = (long) (Math.random() * Long.MAX_VALUE);
+        System.out.println(random);
+        System.out.println(Long.toString(random, 36));
     }
 }
+
